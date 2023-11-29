f445e0029b7e0255de27a5541b8e63346ac46b87
Juan <jfernandez@3pc.de>
2023-11-29 16:39:31 +0100

    feat: change title color

--- src/App.tsx
+++ src/App.tsx
@@ -2,77 +2,75 @@ import { useState } from "react";
 import "./App.css";
 
 const ProgressBar = ({ progressBarWidth }: { progressBarWidth: number }) => {
-	return (
-		<div className="progress-bar__container">
-			<div className="progress-bar__info" style={{ width: `${progressBarWidth}%` }}></div>
-			<span className="progress-bar__span">{Math.floor(progressBarWidth)}%</span>
-			{Math.floor(progressBarWidth) > 99 && <span>Congrats! You've reached 50 words!!</span>}
-		</div>
-	);
+  return (
+    <div className="progress-bar__container">
+      <div className="progress-bar__info" style={{ width: `${progressBarWidth}%` }}></div>
+      <span className="progress-bar__span">{Math.floor(progressBarWidth)}%</span>
+      {Math.floor(progressBarWidth) > 99 && <span>Congrats! You've reached 50 words!!</span>}
+    </div>
+  );
 };
 
 const App = () => {
-	const [progressBarWidth, setProgressBarWidth] = useState<number>(0);
-	const [userInput, setUserInput] = useState<string[]>([]);
-	const characters: number = userInput.join().replaceAll(",", "").length;
-	const words: number = userInput.length > 0 ? userInput[0].split(" ").length : 0;
-	const sentences: number = userInput.length > 0 ? userInput[0].split(".").length : 0;
-	const paragraphs: number = userInput.length > 0 ? userInput[0].split("\n").length : 0;
-	const numberString =
-		userInput.length > 0
-			? userInput[0].split(/[ \n]/).reduce((acc, curr) => {
-					if (curr.length > acc.length) {
-						return curr; // if longest is grater than the nex element in array.
-					} else {
-						return acc; // new element of array is grater than current.
-					}
-			  }, "")
-			: "";
-	const updateUserInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setUserInput(() => [event.target.value]);
-		const calc = (words / 50) * 100;
-		if (calc > 101) {
-			return;
-		}
-		setProgressBarWidth(calc);
-	};
+  const [progressBarWidth, setProgressBarWidth] = useState(0);
+  const [userInput, setUserInput] = useState<string[]>([]);
 
-	const paragraphHandler = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-		if (event.code === "Enter") {
-			setUserInput((prevUserInput) => [...prevUserInput]);
-		}
-	};
+  const characters = userInput.join().replaceAll(",", "").length;
+  const words = userInput.length > 0 ? userInput[0].split(" ").length : 0;
+  const sentences = userInput.length > 0 ? userInput[0].split(".").length : 0;
+  const paragraphs = userInput.length > 0 ? userInput[0].split("\n").length : 0;
+  const numberString =
+    userInput.length > 0
+      ? userInput[0].split(/[ \n]/).reduce((longest, current) => (current.length > longest.length ? current : longest), "")
+      : "";
 
-	return (
-		<main>
-			<section className="home">
-				<header className="navbar">
-					<h1 className="navbar__title">Text Analizer</h1>
-				</header>
-				<div className="user-input">
-					<ul className="user-input__info">
-						<li>
-							Words<p>{words}</p>
-						</li>
-						<li>
-							Characters<p>{characters}</p>
-						</li>
-						<li>
-							Sentences<p>{sentences}</p>
-						</li>
-						<li>
-							Paragraphs<p>{paragraphs}</p>
-						</li>
-					</ul>
-					<textarea className="user-input__input" onChange={updateUserInput} onKeyDown={paragraphHandler} />
-					<span className="user-input__data">
-						Longest Word <br></br><span>{numberString === "" ? "No Words Found" : numberString}</span>
-					</span>
-					<ProgressBar progressBarWidth={progressBarWidth} />
-				</div>
-			</section>
-		</main>
-	);
+  const calculateProgressBarWidth = (words: number) => {
+    const calc = (words / 50) * 100;
+    return calc > 100 ? 100 : calc;
+  };
+
+  const updateUserInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserInput(() => [event.target.value]);
+    setProgressBarWidth(calculateProgressBarWidth(words));
+  };
+
+  const paragraphHandler = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.code === "Enter") {
+      setUserInput((prevUserInput) => [...prevUserInput]);
+    }
+  };
+
+  return (
+    <main>
+      <section className="home">
+        <header className="navbar">
+          <h1 className="navbar__title">Text Analyzer</h1>
+        </header>
+        <div className="user-input">
+          <ul className="user-input__info">
+            <li>
+              Words<p>{words}</p>
+            </li>
+            <li>
+              Characters<p>{characters}</p>
+            </li>
+            <li>
+              Sentences<p>{sentences}</p>
+            </li>
+            <li>
+              Paragraphs<p>{paragraphs}</p>
+            </li>
+          </ul>
+          <textarea className="user-input__input" onChange={updateUserInput} onKeyDown={paragraphHandler} />
+          <span className="user-input__data">
+            Longest Word <br></br>
+            <span>{numberString === "" ? "No Words Found" : numberString}</span>
+          </span>
+          <ProgressBar progressBarWidth={progressBarWidth} />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
